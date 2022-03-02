03d6659ccd85b6507cb733ed44678da49cfa182d
Nathaniel Tang <nathanieltang22@gmail.com>
2022-03-02 14:25:08 -0500

    added EventListeners to buttons

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,14 @@
 //Operator functions
 
-const add = (a,b) => a + b;
+const addition = (a,b) => a + b;
 
-const subtract = (a,b) => a-b;
+const subtraction = (a,b) => a-b;
 
-const multiply = (a,b) => a*b;
+const multiplication = (a,b) => a*b;
 
-const divide = (a,b) => a/b;
+const division = (a,b) => a/b;
 
-const exponent = (a,b) => a**b;
+const power = (a,b) => a**b;
 
 //Operator
 
@@ -19,20 +19,84 @@ function operate(a,b,operator) {
 
     switch (operator) {
         case "+":
-            result = add(a,b);
+            result = addition(a,b);
             break;
         case "-":
-            result = subtract(a,b);
+            result = subtraction(a,b);
             break;
         case "*":
-            result = multiply(a,b);
+            result = multiplication(a,b);
             break;
         case "/":
-            result = divide(a,b);
+            result = division(a,b);
             break;
         case "^":
-            result = exponent(a,b);
+            result = power(a,b);
             break;
     }
     return result;
 }
+
+//global variables
+
+let a = "";
+let b = "";
+let operator = "";
+let temp = "a"
+//variable functions
+
+function variableAssembler(num) {
+    if (temp = "a") {
+        a += num
+    }
+    console.log(a)
+}
+
+function setOperator (op) {
+    operator = op
+    console.log(operator)
+}
+
+//buttons
+
+const one = document.querySelector('#one')
+one.addEventListener('click', () => variableAssembler("1"))
+
+const two = document.querySelector('#two')
+two.addEventListener('click', () => variableAssembler("2"))
+
+const three = document.querySelector('#three')
+three.addEventListener('click', () => variableAssembler("3"))
+
+const four = document.querySelector('#four')
+four.addEventListener('click', () => variableAssembler("4"))
+
+const five = document.querySelector('#five')
+five.addEventListener('click', () => variableAssembler("5"))
+
+const six = document.querySelector('#six');
+six.addEventListener('click', () => variableAssembler("6"));
+
+const seven = document.querySelector('#seven');
+seven.addEventListener('click', () => variableAssembler("7"));
+
+const eight = document.querySelector('#eight');
+eight.addEventListener('click', () => variableAssembler("8"));
+
+const nine = document.querySelector('#nine');
+nine.addEventListener('click', () => variableAssembler("9"));
+
+const add = document.querySelector('#add');
+add.addEventListener('click', () => setOperator('+') )
+
+const subtract = document.querySelector('#subtract');
+subtract.addEventListener('click', () => setOperator('-') )
+
+const multiply = document.querySelector('#multiply');
+multiply.addEventListener('click', () => setOperator('*') )
+
+const divide = document.querySelector('#divide');
+divide.addEventListener('click', () => setOperator('/') )
+
+const exponent = document.querySelector('#exponent');
+exponent.addEventListener('click', () => setOperator('^') )
